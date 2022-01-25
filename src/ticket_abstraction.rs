6e093610d896522c7370e6550ac0c428be710493
Marek Suchánek <msuchane@redhat.com>
2022-01-25 13:33:29 +0100

    Partially implement AbstractTicket for Jira

--- src/ticket_abstraction.rs
+++ src/ticket_abstraction.rs
@@ -23,7 +23,7 @@ pub struct AbstractTicket {
     pub flags: Option<Vec<String>>,
     pub target_release: String,
     pub subsystems: Vec<String>,
-    pub groups: Vec<String>,
+    pub groups: Option<Vec<String>>,
     pub public: bool,
     pub requires_doc_text: DocTextStatus,
     pub duplicates: Vec<AbstractTicket>,
@@ -90,7 +90,81 @@ impl From<Bug> for AbstractTicket {
                 .to_string(),
             // TODO: Implement SST. The path is extra.pool.team.name
             subsystems: vec!["SST".to_string()],
-            groups: bug.groups,
+            groups: Some(bug.groups),
+            // TODO: Implement public
+            public: false,
+            // TODO: Implement RDT
+            requires_doc_text: DocTextStatus::InProgress,
+            duplicates: Vec::new(),
+        }
+    }
+}
+
+impl From<JiraIssue> for AbstractTicket {
+    fn from(issue: JiraIssue) -> Self {
+        AbstractTicket {
+            id: TicketId {
+                key: issue.key,
+                tracker: "Jira".to_string(),
+            },
+            summary: issue.fields.summary,
+            description: issue.fields.description,
+            // TODO: These two fields should be configurable by tracker.
+            // Also, handle the errors properly.
+            doc_type: issue
+                .fields
+                .extra
+                .get("customfield_12317310")
+                .unwrap()
+                .get("value")
+                .unwrap()
+                .as_str()
+                .unwrap()
+                .to_string(),
+            doc_text: issue
+                .fields
+                .extra
+                .get("customfield_12317322")
+                .unwrap()
+                .get("value")
+                .unwrap()
+                .as_str()
+                .unwrap()
+                .to_string(),
+            docs_contact: issue
+                .fields
+                .extra
+                .get("customfield_12317336")
+                .unwrap()
+                .get("emailAddress")
+                .unwrap()
+                .as_str()
+                .unwrap()
+                .to_string(),
+            release_note: None,
+            status: issue.fields.status.name.clone(),
+            is_open: if &issue.fields.status.name == "Closed" {
+                false
+            } else {
+                true
+            },
+            priority: issue.fields.priority.name,
+            url: issue.self_link,
+            assignee: issue.fields.assignee.email_address,
+            // TODO: Implement components.
+            // The call would be issue.fields.components.map(|&c| c.name),
+            // but I'm getting an iterator trait error.
+            components: Vec::new(),
+            product: issue.fields.project.name,
+            labels: Some(issue.fields.labels),
+            // Jira does not support flags
+            flags: None,
+            // TODO: Is the first fix version in the list the one that we want?
+            target_release: issue.fields.fix_versions[0].clone().name,
+            // TODO: Implement SSTs. Previously, we used labels, but now the menu is available.
+            subsystems: vec!["SST".to_string()],
+            // Jira does not recognize groups in the Bugzilla way. This might change.
+            groups: None,
             // TODO: Implement public
             public: false,
             // TODO: Implement RDT
