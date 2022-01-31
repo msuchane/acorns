ecee20e92cca4059d01c8c3ec56d0ea5c48ced1e
Marek Suchánek <msuchane@redhat.com>
2022-01-31 16:30:36 +0100

    Basic processing of the doc text

--- src/note.rs
+++ src/note.rs
@@ -2,6 +2,27 @@ use crate::ticket_abstraction::AbstractTicket;
 
 impl AbstractTicket {
     pub fn release_note(self) -> String {
-        self.doc_text.unwrap_or("No release note.".to_string())
+        let empty = format!(
+            ".🚧 {} | {}\n\n**No release note.** link:{}[]",
+            self.summary, self.docs_contact, self.url
+        );
+        if let Some(ref doc_text) = self.doc_text {
+            if doc_text.trim() == "" {
+                empty
+            } else {
+                format!("{}\n\n({})", doc_text, self.format_signature())
+            }
+        } else {
+            empty
+        }
+    }
+
+    fn format_signature(&self) -> String {
+        let label = format!("{}:{}", self.id.tracker, self.id.key);
+        if self.public {
+            format!("link:{}[{}]", self.url, label)
+        } else {
+            label
+        }
     }
 }
