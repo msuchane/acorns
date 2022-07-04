2f9ec5b54b459fbe9dd924399db6dfb174a3c2ee
Marek Suchánek <msuchane@redhat.com>
2022-07-04 23:44:18 +0200

    Enable full-featured error logging

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,13 @@ use cizrna::cli;
 use cizrna::run;
 
 fn main() -> Result<()> {
+    // Enable full-featured error logging.
+    color_eyre::install()?;
+
+    // Collect command-line arguments.
     let cli_arguments = cli::arguments();
+
+    // Run the main program.
     run(&cli_arguments)?;
 
     Ok(())
