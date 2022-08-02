d957ed92a0d151023675eea40ea1c537b33c8fd2
Marek Suchánek <msuchane@redhat.com>
2022-08-02 19:42:22 +0200

    List sorted releases on the status table

--- src/status_report.rs
+++ src/status_report.rs
@@ -125,6 +125,7 @@ fn email_prefix(email: &str) -> &str {
 }
 
 /// List the products set in the tickets, sorted from most common to least common.
+/// Returns up to 3 most common products and ignores the rest.
 fn combined_products(tickets: &[AbstractTicket]) -> Vec<&str> {
     let products: Counter<&str> = tickets
         .iter()
@@ -132,10 +133,41 @@ fn combined_products(tickets: &[AbstractTicket]) -> Vec<&str> {
         .collect();
 
     products
-        .most_common_ordered()
+        .k_most_common_ordered(3)
         .iter()
         .map(|(elem, _frequency)| *elem)
         .collect()
+}
+
+/// List the releases set in the tickets, sorted from most common to least common.
+/// Returns up to 3 most common releases and ignores the rest.
+fn combined_releases(tickets: &[AbstractTicket]) -> Vec<&str> {
+    let mut releases: Counter<&str> = Counter::new();
+
+    for ticket in tickets.iter() {
+        releases.update(
+            ticket
+                .target_releases
+                .iter()
+                .map(|release| release.as_str()),
+        );
+    }
+
+    releases
+        .k_most_common_ordered(3)
+        .iter()
+        .map(|(elem, _frequency)| *elem)
+        .collect()
+}
+
+/// Display the list of releases or products as a string.
+/// If the list is empty, provide a placeholder instead.
+fn list_or_placeholder(list: &[&str], name: &str) -> String {
+    if list.is_empty() {
+        format!("no {}", name)
+    } else {
+        list.join(", ")
+    }
 }
 
 #[derive(Template)] // this will generate the code...
@@ -152,17 +184,16 @@ struct StatusTableTemplate<'a> {
 
 pub fn analyze_status(tickets: &[AbstractTicket]) -> Result<String> {
     let products = combined_products(tickets);
-    let products_display = if products.is_empty() {
-        "no products".to_string()
-    } else {
-        products.join(", ")
-    };
+    let products_display = list_or_placeholder(&products, "products");
+
+    let releases = combined_releases(tickets);
+    let releases_display = list_or_placeholder(&releases, "releases");
 
     let date_today = Utc::now().to_rfc2822();
 
     let status_table = StatusTableTemplate {
         products: &products_display,
-        release: "9.0",
+        release: &releases_display,
         overall_progress: OverallProgress {
             ..Default::default()
         },
